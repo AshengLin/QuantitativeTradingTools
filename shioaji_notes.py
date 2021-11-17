6c2ce2246e4ea48a35e0e36e27089f8420ce9433
Derek <a0988238060@gmail.com>
2021-11-17 19:50:37 +0800

    feature: order

--- shioaji_notes.py
+++ shioaji_notes.py
@@ -11,9 +11,36 @@ api = sj.Shioaji(simulation=True)  # simulation 測試帳號
 api.login(person_id=os.getenv('YOUR_PERSON_ID'), passwd=os.getenv('YOUR_PASSWORD'),
           contracts_cb=lambda security_type: print(f"{repr(security_type)} fetch done."))
 
+# result = api.activate_ca(
+#     ca_path=os.getenv('YOUR_CA_PATH'),  # 下單電子憑證路徑及檔案名稱
+#     ca_passwd=os.getenv('YOUR_CA_PASS'),  # 下單電子憑證密碼
+#     person_id=os.getenv('YOUR_PERSON_ID'),  # 身份證字號
+# )
+
+
 accounts = api.list_accounts()
 
-k_bars = api.kbars(api.Contracts.Stocks["0050"], start="2010-01-01", end="2021-09-02")  # k棒
+
+contracts = [api.Contracts.Stocks['2330'], api.Contracts.Stocks['2409']]  # 建立一個Contract的List
+# short_stock_sources = api.short_stock_sources(contracts)  # Short Stock Source 查詢股票代碼,或有券張數, 更新時間
+snapshots = api.snapshots(contracts)  # 查詢快照
+# df = pd.DataFrame(snapshots)
+# df.ts = pd.to_datetime(df.ts)
+
+k_bars = api.kbars(api.Contracts.Stocks["2330"], start="2015-09-17",
+                   end="2021-11-17")  # Stocks[] 股票, Futures[] 期貨, Options[] 選擇權, Indexs[] 指標
 df = pd.DataFrame({**k_bars})
+df.ts = pd.to_datetime(df.ts)
+
+
+order = api.Order(price=12, quantity=1,
+                  action=sj.constant.Action.Buy,  # 買進  sj.constant.Action.Sell, #賣出
+                  price_type=sj.constant.StockPriceType.LMT,
+                  order_type=sj.constant.TFTOrderType.ROD,
+                  order_lot=sj.constant.TFTStockOrderLot.Common,
+                  account=api.stock_account)
+contract = api.Contracts.Stocks['2330']
+trade = api.place_order(contract, order)  # 對該contract 進行 order操作
+print(trade)
 
 api.logout()  # 登出
